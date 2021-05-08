d8c78bab6a3c58bfe9e2fe27fe5421809bfda488
Sergey <sergey@mai.com>
2021-05-09 00:45:45 +0300

    the 1-st and the 2-nd tasks

---
+++ hw_7.js
@@ -0,0 +1,28 @@
+// Task_1
+const searchCandidatesByPhoneNumber = phone => {
+    const tmpClientArray = [];
+    let tmpExpectedPhoneNumber = '';
+    for(let i = 0; i < phone.length; i++){
+        if(Number(phone.charAt(i)) >= 0 && phone.charAt(i) !== ' ') tmpExpectedPhoneNumber += phone.charAt(i);
+    }
+    condidateArr.forEach(element => {
+        let tmpActualPhoneNumber = '';
+        for(let i = 0; i < element['phone'].length; i++){
+            if(Number(element['phone'].charAt(i)) >= 0 && element['phone'].charAt(i) !== ' ') tmpActualPhoneNumber += element['phone'].charAt(i);
+        }
+        if(tmpActualPhoneNumber.includes(tmpExpectedPhoneNumber)){
+            tmpClientArray.push(element);
+        }
+    });
+    return tmpClientArray;
+}
+
+// Task_2
+const getCandidateById = id => {
+    let tmpArr = [];
+    condidateArr.forEach(element => {
+    if(element._id === id) Object.assign(tmpArr, element)
+    })
+    tmpArr['registered'] = `${tmpArr['registered'].split('-')[2].split('T')[0]}/${tmpArr['registered'].split('-')[1]}/${tmpArr['registered'].split('-')[0]}`;
+    return tmpArr;
+}
