f47970b56c18fa41d5dd2f7855071aea53dfce22
Sergey <sergey@mai.com>
2021-05-10 17:05:47 +0300

    3d and 4th tasks

--- hw_7.js
+++ hw_7.js
@@ -26,3 +26,31 @@ const getCandidateById = id => {
     tmpArr['registered'] = `${tmpArr['registered'].split('-')[2].split('T')[0]}/${tmpArr['registered'].split('-')[1]}/${tmpArr['registered'].split('-')[0]}`;
     return tmpArr;
 }
+
+// Task_3
+const sortCandidatesArr = sortBy => {
+    const tmpSortArr = [];
+    Object.assign(tmpSortArr, condidateArr)
+    if(!sortBy) return tmpSortArr;
+    if(sortBy === 'asc') return tmpSortArr.sort((a,b) => Number(a['balance'].split('$')[1].split(',')[0] + a['balance'].split('$')[1].split(',')[1]) - Number(b['balance'].split('$')[1].split(',')[0] + b['balance'].split('$')[1].split(',')[1]));
+    if(sortBy === 'desc') return tmpSortArr.sort((a,b) => Number(b['balance'].split('$')[1].split(',')[0] + b['balance'].split('$')[1].split(',')[1]) - Number(a['balance'].split('$')[1].split(',')[0] + a['balance'].split('$')[1].split(',')[1]));
+}
+
+// Task_4*
+const getEyeColorMap = () => {
+    const tmpObject = {};
+    const color = [];
+    condidateArr.forEach(element => {
+        if(!color.includes(element.eyeColor)) color.push(element.eyeColor);
+    });
+
+    color.forEach(element => {
+        const tmpArr = [];
+        condidateArr.forEach(candidatElement => {
+            if(candidatElement.eyeColor === element) tmpArr.push(candidatElement)
+        });
+        tmpObject[element] = tmpArr;
+    });
+
+    return tmpObject;
+}
